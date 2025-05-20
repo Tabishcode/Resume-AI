3f5f84e150cdb1e22b9734ce5f7fa8881bcac755
tabishcode <tabishakhtar007860@gmail.com>
2025-05-21 02:55:41 +0500

    changed middleware file of client

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -4,9 +4,7 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    // Only run middleware on app routes, skip static files and assets
+    "/((?!_next|.*\\..*|favicon.ico).*)",
   ],
 };
