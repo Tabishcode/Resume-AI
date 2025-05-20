cba2d5ad05d86331400b33838514aa3ac1863ae2
tabishcode <tabishakhtar007860@gmail.com>
2025-05-21 04:06:10 +0500

    some fixes

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -3,7 +3,6 @@ import daisyui from 'daisyui';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
